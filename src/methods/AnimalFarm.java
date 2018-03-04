0ea350344161bb595dfd49059dc552ae9e32b66e
J.D.S. Solutions <league@imac-2.attlocal.net>
2018-03-03 17:33:15 -0800

    AnimalFarm DiceRoll IntroToRandom RockPaperScissors SafeCracker

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,7 +18,10 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
-
+	String animal =JOptionPane.showInputDialog(null,"Which animal do you want?" );
+if( animal.equals("duck")){
+	playQuack();
+	}
 	/* 2. Make it so that the user can keep entering new animals. */
 }
 
